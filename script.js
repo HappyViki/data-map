330faa7e8a10a0ec785d57d9c76521208577ec31
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-02-02 17:03:24 -0700

    done with making it pretty

--- script.js
+++ script.js
@@ -1,147 +1,161 @@
-const map = d3.select("#map");
+const map = d3.select('#map')
 
-var margin = {
-		top: 20,
-		right: 20,
-		bottom: 30,
-		left: 40
-	},
-	width = 300 - margin.top - margin.bottom,
-	height = 300 - margin.top - margin.bottom;
+const locations = [
+  {
+    name: '801 Labs',
+    coords: [-111.8828485, 40.7653521]
+  },
+  {
+    name: 'WAVE (Wireless Advanced Vehicle Electrification), Inc.',
+    coords: [-112.0066724, 40.7409764]
+  }
+]
 
-var x = d3.scaleBand()
-	.range([0, width])
-	.padding(.1);
-var y = d3.scaleLinear()
-	.range([height, 0]);
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: 40
+}
+const width = 300 - margin.top - margin.bottom
+const height = 300 - margin.top - margin.bottom
 
-var svg = d3.select("#graph")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform",
-		"translate(" + margin.left + "," + margin.top + ")");
+const x = d3.scaleBand()
+  .range([0, width])
+  .padding(0.1); const y = d3.scaleLinear()
+  .range([height, 0])
 
-d3.json("counties-10m.json").then(function (json) {
+const graph = d3.select('#graph')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform',
+    'translate(' + margin.left + ',' + margin.top + ')')
 
-	const geojson = topojson.feature(json, json.objects.states)
+d3.json('counties-10m.json').then(function (json) {
+  const geojson = topojson.feature(json, json.objects.states)
 
-	const utah = geojson.features.find(state => state.properties.name === "Utah");
+  const projection = d3.geoAlbers()
+    .scale(24000)
+    .translate([5000, 1400])
+  const geoPath = d3.geoPath(projection)
 
-	const center = d3.geoCentroid(utah);
+  const us = map.append('g')
 
-	const projection = d3.geoAlbers()
-		.scale(24000)
-		.translate([5000, 1400]),
-		geoPath = d3.geoPath(projection);
+  us.selectAll('path')
+    .data(geojson.features)
+    .enter()
+    .append('path')
+    .attr('stroke', 'red')
+    .attr('d', geoPath)
 
-	const us = map.append("g");
+  d3.json('Pavement_Condition_Utah_Roads_2004-2012.geojson').then(function (geojson) {
+    const sortedRoadsObj = geojson.features.reduce((result, state) => {
+      if (state.properties.surface_ty === 'Asphalt') {
+        result.asphalt.push(state)
+      } else if (state.properties.surface_ty === 'Concrete') {
+        result.concrete.push(state)
+      } else if (state.properties.surface_ty === 'Gravel') {
+        result.gravel.push(state)
+      }
+      return result
+    }, {
+      asphalt: [],
+      concrete: [],
+      gravel: []
+    })
+    const asphaltRoad = map.append('g')
+    const concreteRoad = map.append('g')
+    const gravelRoad = map.append('g')
 
-	us.selectAll("path")
-		.data(geojson.features)
-		.enter()
-		.append("path")
-		.attr("stroke", "red")
-		.attr("d", geoPath);
+    asphaltRoad.selectAll('path')
+      .data(sortedRoadsObj.asphalt)
+      .enter()
+      .append('path')
+      .attr('stroke', 'green')
+      .attr('d', geoPath)
+      .on('mouseover', function (d) {
+        d3.select('h2').text(d.properties.location)
+      })
 
-	d3.json("Pavement_Condition_Utah_Roads_2004-2012.geojson").then(function (geojson) {
+    concreteRoad.selectAll('path')
+      .data(sortedRoadsObj.concrete)
+      .enter()
+      .append('path')
+      .attr('stroke', 'yellow')
+      .attr('d', geoPath)
+      .on('mouseover', function (d) {
+        d3.select('h2').text(d.properties.location)
+      })
 
-		const sortedRoadsObj = geojson.features.reduce((result, state) => {
-			if (state.properties.surface_ty === "Asphalt") {
-				result.asphalt.push(state)
-			} else if (state.properties.surface_ty === "Concrete") {
-				result.concrete.push(state)
-			} else if (state.properties.surface_ty === "Gravel") {
-				result.gravel.push(state)
-			}
-			return result
-		}, {
-			asphalt: [],
-			concrete: [],
-			gravel: []
-		});
-		const asphaltRoad = map.append("g"),
-			concreteRoad = map.append("g"),
-			gravelRoad = map.append("g");
+    gravelRoad.selectAll('path')
+      .data(sortedRoadsObj.gravel)
+      .enter()
+      .append('path')
+      .attr('stroke', 'red')
+      .attr('d', geoPath)
+      .on('mouseover', function (d) {
+        d3.select('h2').text(d.properties.location)
+      })
 
-		asphaltRoad.selectAll("path")
-			.data(sortedRoadsObj.asphalt)
-			.enter()
-			.append("path")
-			.attr("stroke", "green")
-			.attr("d", geoPath)
-			.attr("class", "road")
-			.on("mouseover", function (d) {
-				d3.select("h2").text(d.properties.location);
-			});
+    const data = [{
+      surfaceType: 'Gravel',
+      SurfaceAmount: sortedRoadsObj.gravel.length
+    },
+    {
+      surfaceType: 'Concrete',
+      SurfaceAmount: sortedRoadsObj.concrete.length
+    },
+    {
+      surfaceType: 'Asphalt',
+      SurfaceAmount: sortedRoadsObj.asphalt.length
+    }
+    ]
 
-		concreteRoad.selectAll("path")
-			.data(sortedRoadsObj.concrete)
-			.enter()
-			.append("path")
-			.attr("stroke", "yellow")
-			.attr("d", geoPath)
-			.attr("class", "road")
-			.on("mouseover", function (d) {
-				d3.select("h2").text(d.properties.location);
-			});
+    data.forEach(function (d) {
+      d.SurfaceAmount = +d.SurfaceAmount
+    })
 
-		gravelRoad.selectAll("path")
-			.data(sortedRoadsObj.gravel)
-			.enter()
-			.append("path")
-			.attr("stroke", "red")
-			.attr("d", geoPath)
-			.attr("class", "road")
-			.on("mouseover", function (d) {
-				d3.select("h2").text(d.properties.location);
-			});
+    x.domain(data.map(function (d) {
+      return d.surfaceType
+    }))
+    y.domain([0, d3.max(data, function (d) {
+      return d.SurfaceAmount
+    })])
 
-		data = [{
-				"surfaceType": "Gravel",
-				"SurfaceAmount": sortedRoadsObj.gravel.length
-			},
-			{
-				"surfaceType": "Concrete",
-				"SurfaceAmount": sortedRoadsObj.concrete.length
-			},
-			{
-				"surfaceType": "Asphalt",
-				"SurfaceAmount": sortedRoadsObj.asphalt.length
-			}
-		]
+    graph.selectAll('.bar')
+      .data(data)
+      .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('x', function (d) {
+        return x(d.surfaceType)
+      })
+      .attr('width', x.bandwidth())
+      .attr('y', function (d) {
+        return y(d.SurfaceAmount)
+      })
+      .attr('height', function (d) {
+        return height - y(d.SurfaceAmount)
+      })
 
-		data.forEach(function (d) {
-			d.SurfaceAmount = +d.SurfaceAmount;
-		});
+    graph.append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x))
 
-		x.domain(data.map(function (d) {
-			return d.surfaceType;
-		}));
-		y.domain([0, d3.max(data, function (d) {
-			return d.SurfaceAmount;
-		})]);
+    graph.append('g')
+      .call(d3.axisLeft(y))
 
-		svg.selectAll(".bar")
-			.data(data)
-			.enter().append("rect")
-			.attr("class", "bar")
-			.attr("x", function (d) {
-				return x(d.surfaceType);
-			})
-			.attr("width", x.bandwidth())
-			.attr("y", function (d) {
-				return y(d.SurfaceAmount);
-			})
-			.attr("height", function (d) {
-				return height - y(d.SurfaceAmount);
-			});
+    const places = map.append('g')
 
-		svg.append("g")
-			.attr("transform", "translate(0," + height + ")")
-			.call(d3.axisBottom(x));
-
-		svg.append("g")
-			.call(d3.axisLeft(y));
-	});
-});
+    places.selectAll('circle')
+      .data(locations).enter()
+      .append('circle')
+      .attr('cx', d => projection(d.coords)[0])
+      .attr('cy', d => projection(d.coords)[1])
+      .attr('r', '8px')
+      .attr('fill', 'red')
+      .on('mouseover', function (d) {
+        d3.select('h2').text(d.name)
+      })
+  })
+})
